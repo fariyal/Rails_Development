f17a8d33723f01bb9fa1d6fcc49dbbac8ad50586
fariyal <fgousepeeran@qwinix.io>
2016-11-11 18:11:14 +0530

    Full stack Development

---
+++ event_schedule/app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+    def new 
+    @user = User.new
+    end
+
+	def index 
+		@users=User.all 
+	end 
+
+	def newform
+		@event=Event.new
+	end
+
+	def create
+  	@user = User.new(user_params )
+  	if @user.save
+    redirect_to log_in_path
+ 	 else
+    render "new"
+    end
+    end
+
+    private 
+	def user_params 
+		params.require(:user).permit(:firstname,:lastname,:username, :email,:phonenumber,:password,:confirmpassword) 
+	end
+end
+
+	
---
+++ event_schedule/config/routes.rb
@@ -0,0 +1,73 @@
+Rails.application.routes.draw do
+
+
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  # root 'welcome#index'
+
+  # Example of regular route:
+  #   get 'products/:id' => 'catalog#view'
+
+
+
+
+get "log_out" => "sessions#destroy", :as => "log_out"
+get "event_page" => "events#new", :as =>"event_page"
+get "event_new_form" => "events#newform", :as => "event_new_form"
+post "event_new_form" => "events#create", :as => "event_new_create"
+get "log_in" => "sessions#new", :as => "log_in"
+post "log_in" => "events#new", :as => "create_event"
+get "sign_up" => "users#new", :as => "sign_up"
+post"sign_up" => "users#create", :as => "saveuser"
+root :to => "users#new"
+# resources :users
+# resources :sessions
+
+  # Example of named route that can be invoked with purchase_url(id: product.id)
+  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+
+  # Example resource route (maps HTTP verbs to controller actions automatically):
+  #   resources :products
+
+  # Example resource route with options:
+  #   resources :products do
+  #     member do
+  #       get 'short'
+  #       post 'toggle'
+  #     end
+  #
+  #     collection do
+  #       get 'sold'
+  #     end
+  #   end
+
+  # Example resource route with sub-resources:
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
+
+  # Example resource route with more complex sub-resources:
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', on: :collection
+  #     end
+  #   end
+
+  # Example resource route with concerns:
+  #   concern :toggleable do
+  #     post 'toggle'
+  #   end
+  #   resources :posts, concerns: :toggleable
+  #   resources :photos, concerns: :toggleable
+
+  # Example resource route within a namespace:
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
+end
